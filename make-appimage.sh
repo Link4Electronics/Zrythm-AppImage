dcd8008d313f26637bf2df68338ca943f9e6cde7
Link4Electronics <link4electronics@gmail.com>
2026-04-21 05:27:23 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -13,7 +13,7 @@ export DESKTOP=/usr/share/applications/org.zrythm.Zrythm.desktop
 export DEPLOY_QT=1
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun /usr/bin/zrythm
 
 # Additional changes can be done in between here
 
