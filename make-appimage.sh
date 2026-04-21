af398280ac7f4cfc9b42f8cbf3d62580ccac300a
Link4Electronics <link4electronics@gmail.com>
2026-04-21 10:49:01 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -9,7 +9,7 @@ export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/scalable/apps/org.zrythm.Zrythm.svg
-export DESKTOP=/usr/share/applications/org.zrythm.Zrythm.desktop
+#export DESKTOP=/usr/share/applications/org.zrythm.Zrythm.desktop
 export DEPLOY_QT=1
 
 # Deploy dependencies /usr/bin/zrythm_launch
